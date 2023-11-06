e10b44c5d827683a981884b6760dc06e7fc44fbb
Seth Parker <csparker247@gmail.com>
2023-11-06 06:45:49 -0500

    cmake-format

--- cmake/Modules/CheckAtomic.cmake
+++ cmake/Modules/CheckAtomic.cmake
@@ -24,28 +24,33 @@ cmake_push_check_state()
 # support lock-free atomics.
 
 # 64-bit target test code
-if (CMAKE_SIZE_OF_VOID_P EQUAL 8) 
-    set(ATOMIC_CODE_64 "
-    std::atomic<uint64_t> x(0);
-    uint64_t i = x.load(std::memory_order_relaxed);
-    ")
-endif() 
+if(CMAKE_SIZE_OF_VOID_P EQUAL 8)
+    set(ATOMIC_CODE_64
+        "
+        std::atomic<uint64_t> x(0);
+        uint64_t i = x.load(std::memory_order_relaxed);
+        ")
+endif()
 
 # Test code
-string(CONFIGURE [[
-    #include <atomic>
-    #include <cstdint>
-    int main() {
-        std::atomic<int> a;
-        std::atomic<short> b;
-        std::atomic<char> c;
-        ++c;
-        ++b;
-        ++a;
-        @ATOMIC_CODE_64@
-        return a;
-    }
-]] ATOMIC_CODE @ONLY)
+string(
+    CONFIGURE
+        [[
+        #include <atomic>
+        #include <cstdint>
+        int main() {
+            std::atomic<int> a;
+            std::atomic<short> b;
+            std::atomic<char> c;
+            ++c;
+            ++b;
+            ++a;
+            @ATOMIC_CODE_64@
+            return a;
+        }
+        ]]
+        ATOMIC_CODE
+    @ONLY)
 
 set(CMAKE_REQUIRED_FLAGS "${CMAKE_REQUIRED_FLAGS} -std=c++11")
 check_cxx_source_compiles("${ATOMIC_CODE}" CXX_ATOMIC_NO_LINK)
@@ -53,19 +58,19 @@ check_cxx_source_compiles("${ATOMIC_CODE}" CXX_ATOMIC_NO_LINK)
 set(ATOMIC_FOUND ${CXX_ATOMIC_NO_LINK})
 
 if(NOT CXX_ATOMIC_NO_LINK)
-  set(CMAKE_REQUIRED_LIBRARIES ${CMAKE_REQUIRED_LIBRARIES} atomic)
-  check_cxx_source_compiles("${ATOMIC_CODE}" CXX_ATOMIC_WITH_LINK)
-  set(ATOMIC_FOUND ${CXX_ATOMIC_WITH_LINK})
+    set(CMAKE_REQUIRED_LIBRARIES ${CMAKE_REQUIRED_LIBRARIES} atomic)
+    check_cxx_source_compiles("${ATOMIC_CODE}" CXX_ATOMIC_WITH_LINK)
+    set(ATOMIC_FOUND ${CXX_ATOMIC_WITH_LINK})
 endif()
 
 cmake_pop_check_state()
 
 if(ATOMIC_FOUND)
-  if(CXX_ATOMIC_NO_LINK)
-    message(VERBOSE "Found std::atomic with no linking")
-  elseif(CXX_ATOMIC_WITH_LINK)
-    message(VERBOSE "Found std::atomic with linking")
-  endif()
+    if(CXX_ATOMIC_NO_LINK)
+        message(VERBOSE "Found std::atomic with no linking")
+    elseif(CXX_ATOMIC_WITH_LINK)
+        message(VERBOSE "Found std::atomic with linking")
+    endif()
 else()
-  message(WARNING "std::atomic not found with compilation checks")
+    message(WARNING "std::atomic not found with compilation checks")
 endif()
